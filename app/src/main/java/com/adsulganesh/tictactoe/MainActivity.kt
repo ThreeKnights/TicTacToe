ce7d8e8b7c30e5ba4906074ae76dce5bf272053c
Ganeshadsul <ganeshadsul76@gmail.com>
2020-01-14 22:04:04 +0530

    Code Optimized.

--- app/src/main/java/com/adsulganesh/tictactoe/MainActivity.kt
+++ app/src/main/java/com/adsulganesh/tictactoe/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity() {
         var cellID:Int = 0
 
         when(buSelected.id){
-
             R.id.bu1 -> cellID = 1
             R.id.bu2 -> cellID = 2
             R.id.bu3 -> cellID = 3
@@ -32,23 +31,18 @@ class MainActivity : AppCompatActivity() {
             R.id.bu7 -> cellID = 7
             R.id.bu8 -> cellID = 8
             R.id.bu9 -> cellID = 9
-
         }
-
         Log.d("buClick:",buSelected.id.toString())
         Log.d("buClick: CellId:",cellID.toString())
-
         playGame(cellID,buSelected)
     }
 
     var activePlayer = 1
-
     var player1 = arrayListOf<Int>()
     var player2 = arrayListOf<Int>()
     var count:Int = 0
 
     fun playGame(cellID:Int, buSelected:Button) {
-        val view:View
         if(activePlayer == 1) {
             buSelected.text = "X"
             buSelected.setBackgroundResource(R.color.lightBlue)
@@ -74,100 +68,41 @@ class MainActivity : AppCompatActivity() {
 
     fun checkWinner() {
         var winner = -1
-        //row one
-        if(player1.contains(1) && player1.contains(2) && player1.contains(3))
-        {
+        if(
+            (player1.contains(1) && player1.contains(2) && player1.contains(3)) ||  //row 1
+            (player1.contains(4) && player1.contains(5) && player1.contains(6)) ||  //row 2
+            (player1.contains(7) && player1.contains(8) && player1.contains(9)) ||  //row 3
+            (player1.contains(1) && player1.contains(4) && player1.contains(7)) ||  //col 1
+            (player1.contains(2) && player1.contains(5) && player1.contains(8)) ||  //col 2
+            (player1.contains(3) && player1.contains(6) && player1.contains(9)) ||  //col 3
+            (player1.contains(1) && player1.contains(5) && player1.contains(9)) ||  //diagonal 1
+            (player1.contains(3) && player1.contains(5) && player1.contains(7))     //diagonal 2
+          )
             winner = 1
-        }
-        if(player2.contains(1) && player2.contains(2) && player2.contains(3))
-        {
+
+
+        if(
+            (player2.contains(1) && player2.contains(2) && player2.contains(3)) ||  //row 1
+            (player2.contains(4) && player2.contains(5) && player2.contains(6)) ||  //row 2
+            (player2.contains(7) && player2.contains(8) && player2.contains(9)) ||  //row 3
+            (player2.contains(1) && player2.contains(4) && player2.contains(7)) ||  //col 1
+            (player2.contains(2) && player2.contains(5) && player2.contains(8)) ||  //col 2
+            (player2.contains(3) && player2.contains(6) && player2.contains(9)) ||  //col 3
+            (player2.contains(1) && player2.contains(5) && player2.contains(9)) ||  //diagonal 1
+            (player2.contains(3) && player2.contains(5) && player2.contains(7))     //diagonal 2
+          )
             winner = 2
-        }
 
-        //row 2
-        if(player1.contains(4) && player1.contains(5) && player1.contains(6))
-        {
-            winner = 1
-        }
-        if(player2.contains(4) && player2.contains(5) && player2.contains(6))
-        {
-            winner = 2
-        }
-
-        //row 3
-        if(player1.contains(7) && player1.contains(8) && player1.contains(9))
-        {
-            winner = 1
-        }
-        if(player2.contains(7) && player2.contains(8) && player2.contains(9))
-        {
-            winner = 2
-        }
-
-
-        //col 1
-        if(player1.contains(1) && player1.contains(4) && player1.contains(7))
-        {
-            winner = 1
-        }
-        if(player2.contains(1) && player2.contains(4) && player2.contains(7))
-        {
-            winner = 2
-        }
-
-        //col 2
-        if(player1.contains(2) && player1.contains(5) && player1.contains(8))
-        {
-            winner = 1
-        }
-        if(player2.contains(2) && player2.contains(5) && player2.contains(8))
-        {
-            winner = 2
-        }
-
-        //col 3
-        if(player1.contains(3) && player1.contains(6) && player1.contains(9))
-        {
-            winner = 1
-        }
-        if(player2.contains(3) && player2.contains(6) && player2.contains(9))
-        {
-            winner = 2
-        }
-
-        //Diagonal 1
-        if(player1.contains(1) && player1.contains(5) && player1.contains(9))
-        {
-            winner = 1
-        }
-        if(player2.contains(1) && player2.contains(5) && player2.contains(9))
-        {
-            winner = 2
-        }
-
-        //Diagonal 2
-        if(player1.contains(3) && player1.contains(5) && player1.contains(7))
-        {
-            winner = 1
-        }
-        if(player2.contains(3) && player2.contains(5) && player2.contains(7))
-        {
-            winner = 2
-        }
-
-        if(winner == 1)
-        {
+        //checking which play won the game
+        if(winner == 1) {
             Toast.makeText(this,"Player 1 Won the game.",Toast.LENGTH_LONG).show()
             winner = -1
             buReset1()
-        }else if(winner == 2)
-        {
+        }else if(winner == 2) {
             Toast.makeText(this,"Player 2 Won the game.",Toast.LENGTH_LONG).show()
             winner = -1
             buReset1()
         }
-
-
     }
 
     fun buReset1() {
@@ -187,8 +122,7 @@ class MainActivity : AppCompatActivity() {
         buttons.add(findViewById(R.id.bu8))
         buttons.add(findViewById(R.id.bu9))
 
-        for (btn in buttons)
-        {
+        for (btn in buttons) {
             btn.isEnabled = true
             btn.text = ""
             btn.setBackgroundResource(R.color.white)
@@ -198,6 +132,4 @@ class MainActivity : AppCompatActivity() {
     fun buReset1(view: View) {
         buReset1()
     }
-
-
 }
